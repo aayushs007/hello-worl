2b495855c4c79b109f0fc1e12d72f7cedcf5be0c
aayushs007 <aayush22singh11@gmail.com>
2024-10-17 18:01:45 +0530

    prints hello world

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf("Enter a number ");
+    scanf("%d",&a);
+    for(int i=0;i<=a;i++){
+        printf("Hello world\n");
+    }
+    return 0;
+}
